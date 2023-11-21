1e72fdf818398481a808cea67f44a1e1ab1ec2bd
Malvina1993 <mykhamalvina@gmail.com>
2023-11-21 23:56:26 +0200

    task 1 and 2

--- src/2.ts
+++ src/2.ts
@@ -16,23 +16,31 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(public name: string, private department: string) {
     this.name = name;
     this.department = department;
-    this.salary = salary;
   }
 
   getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    return `Name: ${this.name}, Department: ${this.department}`;
   }
 }
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, departament: string, private salary: number) {
+    super(name, departament)
+    // this.salary += 10000; точно не зрозуміла де збільшувати потрібно тому додаю два варіанти
+    this.salary = salary;
+
+  }
+
+  increaseSalry() {
+    return this.salary += 10000;
+  }
+  
 }
 
 
