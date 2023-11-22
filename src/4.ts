c588d29662224a8e7a3bc2bede18d31aff85c47c
Malvina1993 <mykhamalvina@gmail.com>
2023-11-22 02:07:10 +0200

    tasks 3 and 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key{
+    private signature: number
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature() {
+        return this.signature;
+    }
+}
+
+class Person{
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House{
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Array<Person>;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key);
+
+}
+
+class MyHouse extends House{
+    openDoor(key:Key) {
+        if (this.key === key) {
+            this.door = true;
+       } 
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
